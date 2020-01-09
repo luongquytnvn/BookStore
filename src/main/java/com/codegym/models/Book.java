c53c64a80cee9ae4ed8855b187e3482d236bccaa
ThinkPad-T450s <kienhayb97>
2020-01-09 09:01:45 +0700

    model book

--- src/main/java/com/codegym/models/Book.java
+++ src/main/java/com/codegym/models/Book.java
@@ -1,4 +1,76 @@
 package com.codegym.models;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+@Entity
 public class Book {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private Number price;
+    private String picture;
+    private String description;
+    private Number amount;
+
+    public Book() {
+    }
+
+    public Book(String name, Number price, String picture, String description, Number amount) {
+        this.name = name;
+        this.price = price;
+        this.picture = picture;
+        this.description = description;
+        this.amount = amount;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Number getPrice() {
+        return price;
+    }
+
+    public void setPrice(Number price) {
+        this.price = price;
+    }
+
+    public String getPicture() {
+        return picture;
+    }
+
+    public void setPicture(String picture) {
+        this.picture = picture;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Number getAmount() {
+        return amount;
+    }
+
+    public void setAmount(Number amount) {
+        this.amount = amount;
+    }
 }
