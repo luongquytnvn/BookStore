e59ccded7d33d1ebac5f816adaf15ef329285166
luongquytnvn <luongquytnvn@gmail.com>
2020-01-09 12:12:43 +0700

    update

--- src/main/java/com/codegym/controllers/UploadFileController.java
+++ src/main/java/com/codegym/controllers/UploadFileController.java
@@ -4,20 +4,20 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.Environment;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.File;
 import java.io.IOException;
 
+@CrossOrigin(origins = "*", maxAge = 3600)
 @RestController
+@RequestMapping("/api/admin")
 public class UploadFileController {
     @Autowired
     Environment env;
 
-    @PostMapping("/api/admin/upload-file")
+    @PostMapping("/upload-file")
     public ResponseEntity<Void> createBook(@RequestParam("file") MultipartFile file) throws IOException {
         long time = System.currentTimeMillis();
         String fileName = time + "-" + file.getOriginalFilename();
