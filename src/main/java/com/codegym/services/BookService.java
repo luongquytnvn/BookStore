3041bf889c318ffcdb3ef6b7e0f4e07f9dfe6e5e
luongquytnvn <luongquytnvn@gmail.com>
2020-01-09 08:45:11 +0700

    fix

--- src/main/java/com/codegym/services/BookService.java
+++ src/main/java/com/codegym/services/BookService.java
@@ -1,9 +1,6 @@
 package com.codegym.services;
 
 import com.codegym.models.Book;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
-import org.springframework.stereotype.Service;
 import java.util.Optional;
 
 public interface BookService {
