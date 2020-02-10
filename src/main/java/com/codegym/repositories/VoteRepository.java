f7e38a0e341e10f21e1186ff6cc2841796176352
luongquytnvn <luongquytnvn@gmail.com>
2020-02-10 16:53:56 +0700

    fix code

--- src/main/java/com/codegym/repositories/VoteRepository.java
+++
@@ -1,9 +0,0 @@
-package com.codegym.repositories;
-
-import com.codegym.models.Vote;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface VoteRepository extends JpaRepository<Vote, Long> {
-}
